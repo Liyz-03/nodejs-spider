f672db9484a10b9bbccf2a0528d419fa3d697f43
Liyz-03 <64033399+Liyz-03@users.noreply.github.com>
2020-08-17 10:08:48 +0800

    Update mysqlconfig.js

--- utils/mysqlconfig.js
+++ utils/mysqlconfig.js
@@ -3,11 +3,11 @@ mysqlconfig = {
 
         host: "localhost", //这是数据库的地址
 
-        user: "root", //需要用户的名字
+        user: "", //需要用户的名字
 
-        password: "root", //用户密码 ，如果你没有密码，直接双引号就是
+        password: "", //用户密码 ，如果你没有密码，直接双引号就是
 
-        database: "jwgl_2" //数据库名字
+        database: "" //数据库名字
 
     } //好了，这样我们就能连接数据库了
 
